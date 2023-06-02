6d7a47f5b61a30d7bb32cf309c060648e2811dbc
Diana1505rojas <diana.rojas2@udea.edu.co>
2023-06-01 22:32:52 -0500

    menu 1 completo

--- main.py
+++ main.py
@@ -118,6 +118,17 @@ while True:
                 print("El equipo ha sido encontrado\n")
                 print(bus)
 
+                salir = str(input("Desea salir ahora ?\n1.SI\n2.NO\n"))
+            while salir !="1" and salir !="2":
+                print("El parametro ingresado no es valido")
+                salir = str(input("Desea salir ahora ?\n1.SI\n2.NO\n"))
+            if salir =="1":
+                print("Ha salido correctamente") 
+                break
+            if salir =="2":
+                print("¡Bienvenido nuevamente al menu principal!\n")
+                pass
+
 
             else:
                 print("El equipo no se encuentra registrado, intenta nuevamente\n")
