1116e269e759e7d6539f02a2b8b5e5dfae828ecb
juanes_10z <jesteban.diez@udea.edu.co>
2023-06-01 21:21:06 -0500

    base de datos responsable

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ while True:
 
             serial = str(input("Ingrese el serial del equipo:\n"))
             num_activo = variable_num(str(input("Ingrese el numero de activo:\n")))
-            nom_equipo = str(input("Ingrese el nombre del equipo:\n"))
-            marca = str(input("Ingrese la marca del equipo:\n"))
+            nom_equipo = nombres(str(input("Ingrese el nombre del equipo:\n")))
+            marca = nombres(str(input("Ingrese la marca del equipo:\n")))
             cod_ubicacion = codigos_num(str(input("Ingrese un codigo de ubicacion contemplado entre 7 y 10 cifras:\n")))
             cod_responsable = codigos_num(str(input("Ingrese codigo del responsable contemplado entre 7 y 10 cifras:\n")))
 
@@ -60,8 +60,8 @@ while True:
 
             if dir:
                 serial = str(input("Ingrese el serial del equipo:\n"))
-                nom_equipo = str(input("Ingrese el nombre del equipo:\n"))
-                marca = str(input("Ingrese la marca del equipo:\n"))
+                nom_equipo = nombres(str(input("Ingrese el nombre del equipo:\n")))
+                marca = nombres(str(input("Ingrese la marca del equipo:\n")))
                 cod_ubicacion = codigos_num(str(input("Ingrese un codigo de ubicacion contemplado entre 7 y 10 cifras:\n")))
                 cod_responsable = codigos_num(str(input("Ingrese codigo del responsable contemplado entre 7 y 10 cifras:\n")))
             
@@ -82,9 +82,6 @@ while True:
                 print("El equipo no se encuentra registrado")
 
 
-                
-
-       
 
         elif equ== "4":
             print("ddd")
@@ -96,6 +93,17 @@ while True:
             print("ddd")
     
 
+
+
+
+
+
+
+
+
+
+
+
     elif men == "2":
         print("Seleccione que desea realizar\n-----------------------------")
         res= menus_secundarios2(str(input("1. Ingresar un nuevo responsable\n2. Ver la información de todos los responsables almacenados \n3. Actualizar la información de un responsable\n4. Eliminar un responsable\n5.Buscar un responsable \n6. Volver al menú principal\n-->  ")))
@@ -105,9 +113,18 @@ while True:
             nombre = nombres(str(input("Ingrese su primer nombre:\n")))
             apellido = nombres(str(input("Ingrese su primer apellido:\n")))
             id = variable_num(str(input("Ingrese su numero de documento de identidad:\n")))
-            cargo = str(input("Ingrese su cargo actual:\n"))
+            cargo = nombres(str(input("Ingrese su cargo actual:\n")))
 
-            print("¡La informacion ha sido guardada exitosamente!")
+            if cod_responsable and nombre and apellido and id and cargo:
+                dir = {
+                    "codigo del responsable":cod_responsable,
+                    "nombre":nombre,
+                    "apellido":apellido,
+                    "ID":id,
+                    "cargo":cargo,
+                }
+                mycol2.insert_one(dir)
+                print("¡La informacion ha sido guardada exitosamente!")
 
 
 
@@ -127,7 +144,7 @@ while True:
 
         if ubi== "1":
             cod_ubicacion = codigos_num(str(input("Ingrese un codigo de ubicacion contemplado entre 7 y 10 cifras:\n")))
-            nom_ubicacion = str(input("Ingrese el nombre de la ubicacion:\n"))
+            nom_ubicacion = nombres(str(input("Ingrese el nombre de la ubicacion:\n")))
             piso_ubicacion = variable_num(str(input("Ingrese el piso donde se encuentra ubicado:\n")))
 
             print("¡La informacion ha sido guardada exitosamente!")
